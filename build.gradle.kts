6ba29d7b3c6056958aa6b0cfa1f90947643de5fd
Kirill Titov <kirill.titov@outlook.com>
2024-04-15 22:14:31 +0300

    add serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.serialization).apply(false)
+//    kotlin("plugin.serialization").version("1.9.20").apply(false)
 }
